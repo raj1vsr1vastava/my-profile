fa181a458a83f8c75937d1af5b7a5dc6a9531d69
Rajiv Srivastava <srivastavar@microsoft.com>
2023-06-22 22:17:44 +0530

    modified

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,7 +6,6 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import Badge from "react-bootstrap/Badge";
-// import ProjectDetailsModal from "./ProjectDetailsModal";
 
 class Experience extends Component {
   render() {
@@ -14,15 +13,6 @@ class Experience extends Component {
       var sectionName = this.props.resumeBasicInfo.section_name.experience;
       var work = this.props.resumeExperience.map(function (work, i) {
         const technologies = work.technologies;
-        // const mainTechnologies = work.mainTech;
-
-        // var mainTech = mainTechnologies.map((technology, i) => {
-        //   return (
-        //     <Badge pill className="main-badge mr-2 mb-2" key={i}>
-        //       {technology}
-        //     </Badge>
-        //   );
-        // });
         var tech = technologies.map((technology, i) => {
           return (
             <Badge pill className="experience-badge mr-2 mb-2" key={i}>
@@ -45,23 +35,16 @@ class Experience extends Component {
                   className="vertical-timeline-element-image"
                   src={work.image}
                   alt="Company"
-                  // height="60"
-                  // width="60"
-                  // style={{marginBottom: 0, paddingBottom: 0, position: 'relative'}}
                 />
             </a>
             }
             key={i}
           >
-            {/* <div style={{ textAlign: "left", marginBottom: "4px" }}>
-              {mainTech}
-            </div> */}
-
             <h3
               className="vertical-timeline-element-title"
               style={{ textAlign: "left" }}
             >
-              {work.company}
+              <b>{work.company}</b>
             </h3>
             <h4
               className="vertical-timeline-element-subtitle"
